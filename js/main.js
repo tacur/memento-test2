3b585cfff190ae61bbf18c84a7b59ee5185d5573
tacur <36847088+tacur@users.noreply.github.com>
2019-10-14 00:04:38 +0200

    dasads

--- js/main.js
+++ js/main.js
@@ -67,7 +67,13 @@
                 $('.back-to-top').fadeOut();
             }
         });
-    });
+	});
+	$(function() {
+		$('a[href*=#]').on('click', function(e) {
+		  e.preventDefault();
+		  $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+		});
+	  });
 
 });
   /*
