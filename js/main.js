17f171ed646099e346c12e9e2f702de484952c57
tacur <36847088+tacur@users.noreply.github.com>
2019-10-13 17:26:17 +0200

    sdsd

--- js/main.js
+++ js/main.js
@@ -1,149 +1,3 @@
-jQuery(document).ready(function($){
-	//cache DOM elements
-	var projectsContainer = $('.cd-projects-container'),
-		projectsPreviewWrapper = projectsContainer.find('.cd-projects-previews'),
-		projectPreviews = projectsPreviewWrapper.children('li'),
-		projects = projectsContainer.find('.cd-projects'),
-		navigationTrigger = $('.cd-nav-trigger'),
-		navigation = $('.cd-primary-nav'),
-		//if browser doesn't support CSS transitions...
-		transitionsNotSupported = ( $('.no-csstransitions').length > 0);
-
-	var animating = false,
-		//will be used to extract random numbers for projects slide up/slide down effect
-		numRandoms = projects.find('li').length, 
-		uniqueRandoms = [];
-
-	//open project
-	projectsPreviewWrapper.on('click', 'a', function(event){
-		event.preventDefault();
-		if( animating == false ) {
-			animating = true;
-			navigationTrigger.add(projectsContainer).addClass('project-open');
-			openProject($(this).parent('li'));
-		}
-	});
-
-	navigationTrigger.on('click', function(event){
-		event.preventDefault();
-		
-		if( animating == false ) {
-			animating = true;
-			if( navigationTrigger.hasClass('project-open') ) {
-				//close visible project
-				navigationTrigger.add(projectsContainer).removeClass('project-open');
-				closeProject();
-			} else if( navigationTrigger.hasClass('nav-visible') ) {
-				//close main navigation
-				navigationTrigger.removeClass('nav-visible');
-				navigation.removeClass('nav-clickable nav-visible');
-				if(transitionsNotSupported) projectPreviews.removeClass('slide-out');
-				else slideToggleProjects(projectsPreviewWrapper.children('li'), -1, 0, false);
-			} else {
-				//open main navigation
-				navigationTrigger.addClass('nav-visible');
-				navigation.addClass('nav-visible');
-				if(transitionsNotSupported) projectPreviews.addClass('slide-out');
-				else slideToggleProjects(projectsPreviewWrapper.children('li'), -1, 0, true);
-			}
-		}	
-
-		if(transitionsNotSupported) animating = false;
-	});
-
-	//scroll down to project info
-	projectsContainer.on('click', '.scroll', function(){
-		projectsContainer.animate({'scrollTop':$(window).height()}, 500); 
-	});
-
-	//check if background-images have been loaded and show project previews
-	projectPreviews.children('a').bgLoaded({
-	  	afterLoaded : function(){
-	   		showPreview(projectPreviews.eq(0));
-	  	}
-	});
-
-	function showPreview(projectPreview) {
-		if(projectPreview.length > 0 ) {
-			setTimeout(function(){
-				projectPreview.addClass('bg-loaded');
-				showPreview(projectPreview.next());
-			}, 150);
-		}
-	}
-
-	function openProject(projectPreview) {
-		var projectIndex = projectPreview.index();
-		projects.children('li').eq(projectIndex).add(projectPreview).addClass('selected');
-		
-		if( transitionsNotSupported ) {
-			projectPreviews.addClass('slide-out').removeClass('selected');
-			projects.children('li').eq(projectIndex).addClass('content-visible');
-			animating = false;
-		} else { 
-			slideToggleProjects(projectPreviews, projectIndex, 0, true);
-		}
-	}
-
-	function closeProject() {
-		projects.find('.selected').removeClass('selected').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-			$(this).removeClass('content-visible').off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-			slideToggleProjects(projectsPreviewWrapper.children('li'), -1, 0, false);
-		});
-
-		//if browser doesn't support CSS transitions...
-		if( transitionsNotSupported ) {
-			projectPreviews.removeClass('slide-out');
-			projects.find('.content-visible').removeClass('content-visible');
-			animating = false;
-		}
-	}
-
-	function slideToggleProjects(projectsPreviewWrapper, projectIndex, index, bool) {
-		if(index == 0 ) createArrayRandom();
-		if( projectIndex != -1 && index == 0 ) index = 1;
-
-		var randomProjectIndex = makeUniqueRandom();
-		if( randomProjectIndex == projectIndex ) randomProjectIndex = makeUniqueRandom();
-		
-		if( index < numRandoms - 1 ) {
-			projectsPreviewWrapper.eq(randomProjectIndex).toggleClass('slide-out', bool);
-			setTimeout( function(){
-				//animate next preview project
-				slideToggleProjects(projectsPreviewWrapper, projectIndex, index + 1, bool);
-			}, 150);
-		} else if ( index == numRandoms - 1 ) {
-			//this is the last project preview to be animated 
-			projectsPreviewWrapper.eq(randomProjectIndex).toggleClass('slide-out', bool).one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-				if( projectIndex != -1) {
-					projects.children('li.selected').addClass('content-visible');
-					projectsPreviewWrapper.eq(projectIndex).addClass('slide-out').removeClass('selected');
-				} else if( navigation.hasClass('nav-visible') && bool ) {
-					navigation.addClass('nav-clickable');
-				}
-				projectsPreviewWrapper.eq(randomProjectIndex).off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-				animating = false;
-			});
-		}
-	}
-
-	//http://stackoverflow.com/questions/19351759/javascript-random-number-out-of-5-no-repeat-until-all-have-been-used
-	function makeUniqueRandom() {
-	    var index = Math.floor(Math.random() * uniqueRandoms.length);
-	    var val = uniqueRandoms[index];
-	    // now remove that value from the array
-	    uniqueRandoms.splice(index, 1);
-	    return val;
-	}
-
-	function createArrayRandom() {
-		//reset array
-		uniqueRandoms.length = 0;
-		for (var i = 0; i < numRandoms; i++) {
-            uniqueRandoms.push(i);
-        }
-	}
-});
 
  /*
  * BG Loaded
@@ -202,6 +56,17 @@ jQuery(document).ready(function($){
                 $('.navbar1').fadeOut();
             }
         });
+	});
+	$(function () {
+        $(window).scroll(function () {
+
+                 // set distance user needs to scroll before we start fadeIn
+            if ($(this).scrollTop() > 100) {
+                $('.back-to-top').fadeIn();
+            } else {
+                $('.back-to-top').fadeOut();
+            }
+        });
     });
 
 });
@@ -229,8 +94,30 @@ jQuery(document).ready(function($){
     });
 
 });*/
+// ####### Frontpage mit Menu/Navbar:
   }(jQuery));
+  (function() {
+	var pushingNavTrigger = document.getElementsByClassName('js-cd-nav-trigger');
+	
+	if(pushingNavTrigger.length > 0) {
+		var mainContent = document.getElementsByClassName('cd-main__content')[0],
+			navAnimating = false;
+		
+		pushingNavTrigger[0].addEventListener('click', function(event) {
+			event.preventDefault();
+			if(navAnimating) return; // already animating -> do not toggle
+			navAnimating = true;
+			Util.toggleClass(document.body, 'nav-is-open', !Util.hasClass(document.body, 'nav-is-open'));
+		});
 
+		mainContent.addEventListener('transitionend', function(){
+			navAnimating = false; // wait for the ened of animation to reset the variable
+		});
+	}
+}());
+
+
+// #######
 (function ($) {
 $(document).ready(function(){
 	// Funktionen für das Scroll-Verhalten
@@ -250,140 +137,27 @@ $(document).ready(function(){
 			return false;
 		});
 	});
-	$("#team").click(function() {
+	$("#menu_nav").click(function() {
     	$('html, body').animate({
-        	scrollTop: $("#team_1").offset().top
+        	scrollTop: $("#team").offset().top
     	}, 2000);
 	});
-	$("#anfahrt").click(function() {
+	$("#anfahrt_nav").click(function() {
     	$('html, body').animate({
-        	scrollTop: $("#anfahrt_1").offset().top
+        	scrollTop: $("#anfahrt").offset().top
     	}, 2000);
 	});
-	$("#kontakt").click(function() {
+	$("#kontakt_nav").click(function() {
     	$('html, body').animate({
-        	scrollTop: $("#kontakt_1").offset().top
+        	scrollTop: $("#kontakt").offset().top
     	}, 2000);
 	});
 
 });
 
-  }(jQuery));
+}(jQuery));
 
 //// Modal NEU Funktionen /////
-jQuery(document).ready(function(){
-	var modalTriggerBts = $('a[data-type="cd-modal-trigger"]'),
-		coverLayer = $('.cd-cover-layer');
-	
-	/*
-		convert a cubic bezier value to a custom mina easing
-		http://stackoverflow.com/questions/25265197/how-to-convert-a-cubic-bezier-value-to-a-custom-mina-easing-snap-svg
-	*/
-	var duration = 600,
-		epsilon = (1000 / 60 / duration) / 4,
-		firstCustomMinaAnimation = bezier(.63,.35,.48,.92, epsilon);
-
-	modalTriggerBts.each(function(){
-		initModal($(this));
-	});
-
-	function initModal(modalTrigger) {
-		var modalTriggerId =  modalTrigger.attr('id'),
-			modal = $('.cd-modal[data-modal="'+ modalTriggerId +'"]'),
-			svgCoverLayer = modal.children('.cd-svg-bg'),
-			paths = svgCoverLayer.find('path'),
-			pathsArray = [];
-		//store Snap objects
-		pathsArray[0] = Snap('#'+paths.eq(0).attr('id')),
-		pathsArray[1] = Snap('#'+paths.eq(1).attr('id')),
-		pathsArray[2] = Snap('#'+paths.eq(2).attr('id'));
-
-		//store path 'd' attribute values	
-		var pathSteps = [];
-		pathSteps[0] = svgCoverLayer.data('step1');
-		pathSteps[1] = svgCoverLayer.data('step2');
-		pathSteps[2] = svgCoverLayer.data('step3');
-		pathSteps[3] = svgCoverLayer.data('step4');
-		pathSteps[4] = svgCoverLayer.data('step5');
-		pathSteps[5] = svgCoverLayer.data('step6');
-		
-		//open modal window
-		modalTrigger.on('click', function(event){
-			event.preventDefault();
-			modal.addClass('modal-is-visible');
-			coverLayer.addClass('modal-is-visible');
-			animateModal(pathsArray, pathSteps, duration, 'open');
-		});
-
-		//close modal window
-		modal.on('click', '.modal-close', function(event){
-			event.preventDefault();
-			modal.removeClass('modal-is-visible');
-			coverLayer.removeClass('modal-is-visible');
-			animateModal(pathsArray, pathSteps, duration, 'close');
-		});
-	}
-
-	function animateModal(paths, pathSteps, duration, animationType) {
-		var path1 = ( animationType == 'open' ) ? pathSteps[1] : pathSteps[0],
-			path2 = ( animationType == 'open' ) ? pathSteps[3] : pathSteps[2],
-			path3 = ( animationType == 'open' ) ? pathSteps[5] : pathSteps[4];
-		paths[0].animate({'d': path1}, duration, firstCustomMinaAnimation);
-		paths[1].animate({'d': path2}, duration, firstCustomMinaAnimation);
-		paths[2].animate({'d': path3}, duration, firstCustomMinaAnimation);
-	}
-
-	function bezier(x1, y1, x2, y2, epsilon){
-		//https://github.com/arian/cubic-bezier
-		var curveX = function(t){
-			var v = 1 - t;
-			return 3 * v * v * t * x1 + 3 * v * t * t * x2 + t * t * t;
-		};
-
-		var curveY = function(t){
-			var v = 1 - t;
-			return 3 * v * v * t * y1 + 3 * v * t * t * y2 + t * t * t;
-		};
-
-		var derivativeCurveX = function(t){
-			var v = 1 - t;
-			return 3 * (2 * (t - 1) * t + v * v) * x1 + 3 * (- t * t * t + 2 * v * t) * x2;
-		};
-
-		return function(t){
-
-			var x = t, t0, t1, t2, x2, d2, i;
-
-			// First try a few iterations of Newton's method -- normally very fast.
-			for (t2 = x, i = 0; i < 8; i++){
-				x2 = curveX(t2) - x;
-				if (Math.abs(x2) < epsilon) return curveY(t2);
-				d2 = derivativeCurveX(t2);
-				if (Math.abs(d2) < 1e-6) break;
-				t2 = t2 - x2 / d2;
-			}
-
-			t0 = 0, t1 = 1, t2 = x;
-
-			if (t2 < t0) return curveY(t0);
-			if (t2 > t1) return curveY(t1);
-
-			// Fallback to the bisection method for reliability.
-			while (t0 < t1){
-				x2 = curveX(t2);
-				if (Math.abs(x2 - x) < epsilon) return curveY(t2);
-				if (x > x2) t0 = t2;
-				else t1 = t2;
-				t2 = (t1 - t0) * .5 + t0;
-			}
-
-			// Failure
-			return curveY(t2);
-
-		};
-	};
-});
-
 jQuery(document).ready(function(){
 	var modalTriggerBts = $('a[data-type="cd-modal-trigger"]'),
 		coverLayer = $('.cd-cover-layer');
